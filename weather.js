2928a4d03104a4d0fbc27d978adc4c929df0a866
Rakesh <rakesh.h7997@gmail.com>
2023-11-27 14:36:40 +0530

    trouble with searchbox

--- weather.js
+++ weather.js
@@ -1,15 +1,21 @@
 
 const apiKey = "6a78ae62ec02d1f0b84a65e74bce6ece";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Bangalore&units=metric";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather() {
-    const responce = await fetch(apiUrl +`&appid=${apiKey}`);
+const searchBox =document.querySelector(".search input");
+const searchBtn =document.querySelector(".search button");
+
+async function checkWeather(city) {
+    const responce = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await responce.json();
     
     console.log(data);
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = data.main.temp + "°c";
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity = "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 } 
-checkWeather();
+
+  searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+})
