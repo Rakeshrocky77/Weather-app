07b297127c04961c37fcae57b8970b987d3cd3b7
Rakesh <rakesh.h7997@gmail.com>
2023-11-27 14:10:19 +0530

    add temperature

--- weather.js
+++ weather.js
@@ -1,3 +1,15 @@
+
+const apiKey = "6a78ae62ec02d1f0b84a65e74bce6ece";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Bangalore&units=metric";
+
 async function checkWeather() {
-    const responce = await fetch(apiUrl +1);    
-}
+    const responce = await fetch(apiUrl +`&appid=${apiKey}`);
+    var data = await responce.json();
+    
+    console.log(data);
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = data.main.temp + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity = "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+} 
+checkWeather();
